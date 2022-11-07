cca488b20921601341a473611c4305937524f5a9
Tom <elunico@users.noreply.github.com>
2022-11-06 20:39:16 -0500

    Initial commit

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define BUFSIZE 4096
+
+int main()
+{
+    char buffer[BUFSIZE];
+    int red;
+    int offset = 0;
+
+    int last_line = 0;
+
+    while ((red = getchar()) != EOF) {
+        if (offset == BUFSIZE) {
+            printf("%s", buffer);
+            offset = 0;
+        }
+        if (red == '\n' || red == '\r') {
+            continue;
+        }
+        if (red == ' ' && last_line == 0) {
+            buffer[offset] = '\0';
+            printf("%s\n", buffer);
+            offset = 0;
+            last_line = 1;
+        } else {
+            buffer[offset] = (char)red;
+            offset++;
+            last_line = 0;
+        }
+    }
+    if (offset > 0) {
+        printf("%s\n", buffer);
+    }
+
+
+    return 0;
+}
